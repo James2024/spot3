8d1cb70c6f7ed25beda00d4bb80d9a4751f5862f
James2024 <james.symes@yahoo.co.uk>
2020-03-07 21:06:06 +0000

    Industry changes
    Germany:
    6 more civs from civ focus'
    
    USSR:
    6 more civs
    
    Germany:
    8 more mills
    7 more civs
    
    Supply zones changed on the stalin line
    
    attempted to import horst fuel system

--- common/defines/autism_defines.lua
+++ common/defines/autism_defines.lua
@@ -1,1 +1,2 @@
 NDefines.NMilitary.ANTI_AIR_ATTACK_TO_AMOUNT = 0.001
+NDefines.NGame.GAME_SPEED_SECONDS = { 0.375, 0.175, 0.1, 0.035, 0.0 } -- game speeds for each level. Must be 5 entries with last one 0 for unbound
