919c9d647a7b2e5bde94e6685af6e3ccae2ff2c7
James2024 <james.symes@yahoo.co.uk>
2020-04-02 02:04:46 +0100

    Agency / Supp Tech / focus cleanup
    Agencies:
    Reduced to 0 cost and 80 days
    
    Supp tech:
    Fixed to be with 1.9.1
    
    USA:
    Brought back to old spot for a while as work is done
    
    Spain:
    Made it the same in horst with a potential error fix?
    Added spanish events so no civil war and xp spawn ins for other nations.
    
    Italy:
    Brought in old spot italy tree

--- common/defines/autism_defines.lua
+++ common/defines/autism_defines.lua
@@ -1,2 +1,6 @@
 NDefines.NMilitary.ANTI_AIR_ATTACK_TO_AMOUNT = 0.001
 NDefines.NGame.GAME_SPEED_SECONDS = { 0.375, 0.175, 0.1, 0.035, 0.0 } -- game speeds for each level. Must be 5 entries with last one 0 for unbound
+
+AGENCY_CREATION_FACTORIES = 0
+AGENCY_CREATION_DAYS = 0
+AGENCY_UPGRADE_DAYS = 80
